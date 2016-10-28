8d4a997979a6cf0adc8bbf47c6a81bab14fa9eaf
Jordan Zucker <jordan.zucker@gmail.com>
2016-10-28 12:19:19 -0700

    Pretty good prototype assembled

---
+++ ClientInAppDelegateObjC/ClientInAppDelegateObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ClientInAppDelegateObjC
+//
+//  Created by Jordan Zucker on 10/28/16.
+//  Copyright © 2016 PubNub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,8 @@
+project 'ClientInAppDelegateObjC/ClientInAppDelegateObjC'
+workspace 'AppleExamples'
+
+use_frameworks!
+
+target 'ClientInAppDelegateObjC' do
+	pod 'PubNub'
+end
